c2c1c2a6e5d70f59873b64c6aad0e619cb0e1777
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2023-12-27 14:27:52 +0100

    create upload_dir if it does not exist

--- webook_html_generator/api.py
+++ webook_html_generator/api.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 
@@ -10,22 +12,32 @@ from webook_html_generator.config import Config
 from webook_html_generator.generator import Generator
 
 
+def load_config() -> Config:
+    dotenv_path = Path("../.env")
+    load_dotenv(dotenv_path=dotenv_path)
+    return Config()
+
+
+config: Config = load_config()
+os.makedirs(config.upload_dir, exist_ok=True)
+
 def run_rsync_to_bucket():
     """Run rsync to sync the upload_dir with the google cloud bucket"""
-    os.system(f"gsutil rsync -r {Config.upload_dir} gs://{Config.google_cloud_bucket}")
+    os.system(f"gsutil rsync -r {config.upload_dir} gs://{config.google_cloud_bucket}")
 
 
 def sync_from_bucket():
     """Sync the upload_dir with the google cloud bucket"""
-    os.system(f"gsutil rsync -r gs://{Config.google_cloud_bucket} {Config.upload_dir}")
+    os.system(f"gsutil rsync -r gs://{config.google_cloud_bucket} {config.upload_dir}")
 
 
 app = FastAPI(title="htmlgenerator")
 app.mount(
-    "/screendisplay", StaticFiles(directory=Config.upload_dir), name="screendisplay"
+    "/screendisplay", StaticFiles(directory=config.upload_dir), name="screendisplay"
 )
 
 sync_from_bucket()
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
