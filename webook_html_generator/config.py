2507b6aa1eb58301444168824ae0f28e1f4fc0f7
DESKTOP-T8VR2O2\Magnus Harlem <magnus@wemade.no>
2023-12-28 13:26:25 +0100

    set default values for endpoints in config.py

--- webook_html_generator/config.py
+++ webook_html_generator/config.py
@@ -8,12 +8,16 @@ class Config:
         self.user_name = os.getenv("USER_NAME")
         self.password = os.getenv("PASSWORD")
         self.default_request_time = os.getenv("DEFAULT_REQUESTS_TIMEOUT")
-        self.next_events_url = os.getenv("NEXT_EVENTS_URL")
-        self.current_events_url = os.getenv("CURRENT_EVENTS_URL")
-        self.screens_url = os.getenv("SCREEN_URL")
-        self.rooms_url = os.getenv("ROOM_URL")
-        self.locations_url = os.getenv("LOCATION_URL")
-        self.layout_url = os.getenv("LAYOUT_URL")
+        self.next_events_url = os.getenv(
+            "NEXT_EVENTS_URL", "events/next_on_schedule?days_ahead=6&limit=1000"
+        )
+        self.current_events_url = os.getenv("CURRENT_EVENTS_URL", "events/current")
+        self.screens_url = os.getenv(
+            "SCREENS_URL", "screenresources?offset=0&limit=100"
+        )
+        self.rooms_url = os.getenv("ROOMS_URL", "rooms?offset=0&limit=100")
+        self.locations_url = os.getenv("LOCATIONS_URL", "locations?offset=0&limit=100")
+        self.layout_url = os.getenv("LAYOUT_URL", "displaylayouts?offset=0&limit=100")
         self.scheduler_interval_in_sec = os.getenv("SCHEDULER_INTERVAL_SEC")
         self.scheduler_interval_in_min = os.getenv("SCHEDULER_INTERVAL_MIN")
         self.upload_dir = os.getenv("UPLOAD_DIR")
